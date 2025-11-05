2e0da279a37b8f76476575c93997d1c73eeffec7
huynvn <Huy.Nguyen@navigosgroup.com>
2025-11-05 17:57:40 +0700

    Update code

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -1,97 +1,108 @@
 import { Dialog } from '@headlessui/react';
-import { useRef, useState } from 'react';
+import { collection, getDocs, query } from 'firebase/firestore';
+import { useEffect, useRef, useState } from 'react';
 import { useAuthState } from '~/components/contexts/UserContext';
 import { SignInButton } from '~/components/domain/auth/SignInButton';
 import { SignOutButton } from '~/components/domain/auth/SignOutButton';
 import { Head } from '~/components/shared/Head';
+import { useFirestore } from '~/lib/firebase';
+
+type Tool = {
+  id: string;
+  title: string;
+  description: string;
+  url: string;
+};
+
+enum InputEnum {
+  ID = 'id',
+  TITLE = 'title',
+  DESCRIPTION = 'description',
+  URL = 'url',
+}
 
 function Index() {
-  const { state } = useAuthState();
-  const [isOpen, setIsOpen] = useState(true);
-  const completeButtonRef = useRef(null);
+  const [tools, setTools] = useState<Array<Tool>>([]);
+  const firestore = useFirestore();
+  const [inputData, setInputData] = useState<Partial<Tool>>({
+    title: '',
+    description: '',
+    url: '',
+  });
+  useEffect(() => {
+    async function fetchData() {
+      const toolsCollection = collection(firestore, 'tools');
+      const toolsQuery = query(toolsCollection);
+      const querySnapshot = await getDocs(toolsQuery);
+      const fetchedData: Array<Tool> = [];
+
+      querySnapshot.forEach((doc) => {
+        fetchedData.push({ id: doc.id, ...doc.data() } as Tool);
+      });
+
+      setTools(fetchedData);
+    }
+    fetchData();
+  }, []);
+
+  const handleInputChange = (field: InputEnum, value: string) => {
+    setInputData({ ...inputData, [field]: value });
+  };
 
   return (
     <>
       <Head title="TOP PAGE" />
-      <div className="hero min-h-screen">
-        <div className="text-center hero-content">
-          <div>
-            <h1 className="text-3xl font-bold">
-              <a className="link link-primary" target="_blank" href="https://vitejs.dev/" rel="noreferrer">
-                Vite
-              </a>{' '}
-              +{' '}
-              <a className="link link-primary" target="_blank" href="https://reactjs.org/" rel="noreferrer">
-                React
-              </a>{' '}
-              +{' '}
-              <a className="link link-primary" target="_blank" href="https://www.typescriptlang.org/" rel="noreferrer">
-                TypeScript
-              </a>{' '}
-              +{' '}
-              <a className="link link-primary" target="_blank" href="https://tailwindcss.com/" rel="noreferrer">
-                TailwindCSS
-              </a>{' '}
-              Starter
-            </h1>
-            <p className="mt-4 text-lg">
-              For fast <b>prototyping</b>. Already set up{' '}
-              <a
-                className="link link-primary"
-                target="_blank"
-                href="https://github.com/firebase/firebase-js-sdk"
-                rel="noreferrer"
-              >
-                Firebase(v9)
-              </a>
-              ,{' '}
-              <a className="link link-primary" target="_blank" href="https://daisyui.com/" rel="noreferrer">
-                daisyUI
-              </a>
-              ,{' '}
-              <a className="link link-primary" target="_blank" href="https://github.com/eslint/eslint" rel="noreferrer">
-                ESLint
-              </a>
-              ,{' '}
-              <a
-                className="link link-primary"
-                target="_blank"
-                href="https://github.com/prettier/prettier"
-                rel="noreferrer"
-              >
-                Prettier
-              </a>
-              .
-            </p>
-            <div className="mt-4 grid gap-2">
-              {state.state === 'UNKNOWN' ? null : state.state === 'SIGNED_OUT' ? <SignInButton /> : <SignOutButton />}
-              <button onClick={() => setIsOpen(true)}>Display Dialog</button>
-            </div>
-          </div>
+      <div className="hero min-h-screen bg-slate-800">
+        <div className="max-w-5xl mx-auto">
+          <form className="flex">
+            <input
+              type="text"
+              placeholder="title"
+              onChange={(e) => handleInputChange(InputEnum.TITLE, e.target.value)}
+              value={inputData.title}
+              className="m-4 bg-transparent border border-slate-700 focus:ring-slate-400 focus:outlie-none p-4 rounded-lg text-slate-50"
+            />
+            <input
+              type="text"
+              placeholder="description"
+              onChange={(e) => handleInputChange(InputEnum.DESCRIPTION, e.target.value)}
+              value={inputData.description}
+              className="m-4 bg-transparent border border-slate-700 focus:ring-slate-400 focus:outlie-none p-4 rounded-lg text-slate-50"
+            />
+            <input
+              type="text"
+              placeholder="url"
+              onChange={(e) => handleInputChange(InputEnum.URL, e.target.value)}
+              value={inputData.url}
+              className="m-4 bg-transparent border border-slate-700 focus:ring-slate-400 focus:outlie-none p-4 rounded-lg text-slate-50"
+            />
+            <button
+              type="submit"
+              className="m-4 border border-purple-500 p-5 rounded-lg bg-purple-600 bg-opacity-30 hover:bg-opacity-50 text-slate-50 transition-opacity"
+            >
+              Add New Tool
+            </button>
+          </form>
+          <table className="table w-full bg-transparent text-slate-50">
+            <thead>
+              <tr>
+                <th className="bg-slate-900 border border-slate-700 text-slate-50">Title</th>
+                <th className="bg-slate-900 border border-slate-700 text-slate-50">Description</th>
+                <th className="bg-slate-900 border border-slate-700 text-slate-50">Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tools.map((tool) => (
+                <tr key={tool.id}>
+                  <td className="bg-slate-800 border border-slate-700">{tool.title}</td>
+                  <td className="bg-slate-800 border border-slate-700">{tool.description}</td>
+                  <td className="bg-slate-800 border border-slate-700"> {tool.url}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-      <Dialog
-        className="flex fixed inset-0 z-10 overflow-y-auto"
-        initialFocus={completeButtonRef}
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-      >
-        <div className="flex items-center justify-center min-h-screen w-screen">
-          <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-          <div className="relative bg-white rounded max-w-120 p-8 mx-auto">
-            <Dialog.Title>Dialog Title</Dialog.Title>
-            <Dialog.Description>Dialog description</Dialog.Description>
-            <button
-              ref={completeButtonRef}
-              type="button"
-              className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-              onClick={() => setIsOpen(false)}
-            >
-              Got it, thanks!
-            </button>
-          </div>
-        </div>
-      </Dialog>
     </>
   );
 }
