58a195814223bcd0bad78885c5d6e48da2651432
huynvn010193 <hpn010193@gmail.com>
2025-11-17 23:28:32 +0700

    update code

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -8,6 +8,7 @@ import { Head } from '~/components/shared/Head';
 import { useFirestore } from '~/lib/firebase';
 import { ToastContainer, toast } from 'react-toastify';
 import { PencilSquareIcon } from '@heroicons/react/24/outline';
+import ToolCard from '../shared/ToolCard';
 
 export type Tool = {
   id: string;
@@ -118,19 +119,7 @@ function Index() {
           </form>
           <div className="grid grid-cols-3 gap-4 w-full bg-transparent text-slate-50">
             {tools.map((tool) => (
-              <div
-                key={tool.id}
-                className="h-48 group relative flex flex-col justify-between rounded-md shadow-slate-900 shadow-md p-4 bg-gradient-to-r from-slate-800 to-slate-700"
-              >
-                <div>
-                  <div className="text-xl mb-2 font-bold">{tool.title}</div>
-                  <div className="">{tool.description}</div>
-                </div>
-                <a className="text-slate-200" href={tool.url}>
-                  {tool.url}
-                </a>
-                <PencilSquareIcon className="h-6 w-6 text-blue-500 hidden group-hover:block absolute top-4 right-4 cursor-pointer" />
-              </div>
+              <ToolCard key={tool.id} tool={tool} />
             ))}
           </div>
         </div>
--- src/components/shared/ToolCard.tsx
+++ src/components/shared/ToolCard.tsx
@@ -1,12 +1,18 @@
-import { PencilSquareIcon } from '@heroicons/react/24/outline';
+import { PencilSquareIcon, CheckIcon, XCircleIcon } from '@heroicons/react/24/outline';
 import * as React from 'react';
 import { Tool } from '../screens/Index';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export interface IToolCardProps {
   tool: Tool;
 }
 
 export default function ToolCard({ tool }: IToolCardProps) {
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const toggleIsEdit = () => {
+    setIsEdit((prevIsEdit) => !prevIsEdit);
+  };
   return (
     <div
       key={tool.id}
@@ -19,7 +25,17 @@ export default function ToolCard({ tool }: IToolCardProps) {
       <a className="text-slate-200" href={tool.url}>
         {tool.url}
       </a>
-      <PencilSquareIcon className="h-6 w-6 text-blue-500 hidden group-hover:block absolute top-4 right-4 cursor-pointer" />
+
+      {isEdit ? (
+        <>
+          <CheckIcon className="h-6 w-6 text-green-500 absolute top-4 right-4 cursor-pointer" />
+          <XCircleIcon className="h-6 w-6 text-red-500 absolute top-4 right-8 cursor-pointer" />
+        </>
+      ) : (
+        <button className="btn btn-actice btn-ghost" onClick={toggleIsEdit}>
+          <PencilSquareIcon className="h-6 w-6 text-slate-50 hidden group-hover:block absolute top-4 right-4 cursor-pointer" />
+        </button>
+      )}
     </div>
   );
 }
