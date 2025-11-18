3c717d2c9936f5a0626a3552ff21143aa371c953
huynvn010193 <hpn010193@gmail.com>
2025-11-18 23:17:35 +0700

    Update code UI

--- src/components/shared/ToolCard.tsx
+++ src/components/shared/ToolCard.tsx
@@ -13,27 +13,51 @@ export default function ToolCard({ tool }: IToolCardProps) {
   const toggleIsEdit = () => {
     setIsEdit((prevIsEdit) => !prevIsEdit);
   };
+
+  const [inputData, setInputData] = useState<Partial<Tool>>(tool);
+
+  const inputClasses = clsx('bg-transparent', 'border-0', 'cursor-pointer', 'py-2', 'px-4', 'rounded-md');
+
   return (
     <div
-      key={tool.id}
+      key={inputData.id}
       className="h-48 group relative flex flex-col justify-between rounded-md shadow-slate-900 shadow-md p-4 bg-gradient-to-r from-slate-800 to-slate-700"
     >
       <div>
-        <div className="text-xl mb-2 font-bold">{tool.title}</div>
-        <div className="">{tool.description}</div>
+        <input
+          className={clsx(inputClasses, 'text-xl mb-2 font-bold text-slate-500', {
+            'bg-gray-900': isEdit,
+            'cursor-text': isEdit,
+          })}
+          value={inputData.title}
+        />
+        <input
+          className={clsx(inputClasses, {
+            'bg-gray-900': isEdit,
+            'cursor-text': isEdit,
+          })}
+          value={inputData.description}
+        />
       </div>
-      <a className="text-slate-200" href={tool.url}>
-        {tool.url}
-      </a>
+      <input
+        className={clsx(inputClasses, 'text-slate-200', {
+          'bg-gray-900': isEdit,
+          'cursor-text': isEdit,
+        })}
+        value={inputData.url}
+      />
 
       {isEdit ? (
         <>
-          <CheckIcon className="h-6 w-6 text-green-500 absolute top-4 right-4 cursor-pointer" />
-          <XCircleIcon className="h-6 w-6 text-red-500 absolute top-4 right-8 cursor-pointer" />
+          <CheckIcon className="h-6 w-6 text-green-500 absolute top-4 right-12 cursor-pointer" />
+          <XCircleIcon className="h-6 w-6 text-red-500 absolute top-4 right-4 cursor-pointer" />
         </>
       ) : (
-        <button className="btn btn-actice btn-ghost" onClick={toggleIsEdit}>
-          <PencilSquareIcon className="h-6 w-6 text-slate-50 hidden group-hover:block absolute top-4 right-4 cursor-pointer" />
+        <button
+          className="btn btn-actice btn-ghost hidden group-hover:block absolute top-4 right-4"
+          onClick={toggleIsEdit}
+        >
+          <PencilSquareIcon className="h-6 w-6 text-slate-50 cursor-pointer" />
         </button>
       )}
     </div>
